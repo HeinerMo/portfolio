d6a0d2ecbec438f9f1d1052da6168f2586499779
Heiner <heinermongelopez@gmail.com>
2023-02-07 10:48:58 -0600

    Fixed alingment issues with navbar and developer portrait

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,16 +12,14 @@ export class HomeComponent implements OnInit {
   learning : Technology[] = [];
   
   constructor() { 
-    this.technologies.push(new Technology({text: "Android Development with Android Studio", logoPath:"/assets/img/android_logo.png"}))
-    this.technologies.push(new Technology({text: "Java Standalone Applications", logoPath:"/assets/img/java_logo.png"}))
-    this.technologies.push(new Technology({text: "Python Scripting and Interaction with Arduino Boards.", logoPath:"/assets/img/python_logo.png"}))
-    this.technologies.push(new Technology({text: "Angular Frontend Development", logoPath:"/assets/img/angular_logo.png"}))
-    this.technologies.push(new Technology({text: "Linux", logoPath:"/assets/img/linux_logo.png"}))
-    this.technologies.push(new Technology({text: "Arduino", logoPath:"/assets/img/arduino_logo.png"}))
+    this.technologies.push(new Technology({text: "Android Development with Android Studio.", logoPath:"/assets/img/android_logo.png"}))
+    this.technologies.push(new Technology({text: "Java Standalone Applications.", logoPath:"/assets/img/java_logo.png"}))
+    this.technologies.push(new Technology({text: "Python Scripting to interact with Arduino Boards.", logoPath:"/assets/img/python_logo.png"}))
+    this.technologies.push(new Technology({text: "Angular Frontend Development.", logoPath:"/assets/img/angular_logo.png"}))
+    this.technologies.push(new Technology({text: "Bash Command Line Interface.", logoPath:"/assets/img/bash_logo.png"}))
+    this.technologies.push(new Technology({text: "Arduino Prototyping and Automation.", logoPath:"/assets/img/arduino_logo.png"}))
 
-    this.learning.push(new Technology({text: "Kotlin", logoPath:"/assets/img/"}))
-    this.learning.push(new Technology({text: "Spring", logoPath:"/assets/img/"}))
-    this.learning.push(new Technology({text: "DJango", logoPath:"/assets/img/"}))
+    this.learning.push(new Technology({text: "Kotlin for Android Development.", logoPath:"/assets/img/kotlin_logo.png"}))
   }
 
   ngOnInit(): void {
