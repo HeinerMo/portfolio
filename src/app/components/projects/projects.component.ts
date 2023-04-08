d62236c2ab8f2d03cfa47f4715a6b8e754f9f216
Heiner Monge <heinermongelopez@gmail.com>
2023-04-07 18:51:31 -0600

    Added new project

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -14,6 +14,7 @@ export class ProjectsComponent implements OnInit {
 
   constructor() {
     this.projects.push(new Project({ name: 'Task Tracker', description: "Task Tracker is a simple android application that aims to help the users keep a list of all the tasks they should complete while being as simple and non intrusive as possible.", repositoryUrl: 'https://github.com/HeinerMo/TaskTracker', icon: 'assets/projects/tasktracker/task_tracker_icon.png' }))
+    this.projects.push(new Project({ name: 'Questionnaire System', description: "A system that allows an organization to apply surveys to their employees and people outside the company's network.", repositoryUrl: 'https://github.com/HeinerMo/Questionnaires', icon: 'assets/projects/questionnaire/questionnaire.png' }))
   }
 
   ngOnInit(): void {
