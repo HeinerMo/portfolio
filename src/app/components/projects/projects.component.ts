2513d9979d2edab30b12ef8fec8629a2797d6127
Heiner Monge <heinermongelopez@gmail.com>
2023-05-07 19:54:18 -0600

    Added TC645 project

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -13,8 +13,9 @@ export class ProjectsComponent implements OnInit {
   handsetData: HandsetData = HandsetData.getInstance()
 
   constructor() {
-    this.projects.push(new Project({ name: 'Task Tracker', description: "Task Tracker is a simple android application that aims to help the users keep a list of all the tasks they should complete while being as simple and non intrusive as possible.", repositoryUrl: 'https://github.com/HeinerMo/TaskTracker', icon: 'assets/projects/tasktracker/task_tracker_icon.png' }))
-    this.projects.push(new Project({ name: 'Questionnaire System', description: "A system that allows an organization to apply surveys to their employees and people outside the company's network.", repositoryUrl: 'https://github.com/HeinerMo/Questionnaires', icon: 'assets/projects/questionnaire/questionnaire.png' }))
+    this.projects.push(new Project({ name: 'TC-645 Website', description: "A website is designed and developed for the TC-645 community work project at the University of Costa Rica. The website follows the visual identity standards of the university and complies with the regulations established therein.", repositoryUrl: 'https://tcu645.vercel.app/'}))
+    this.projects.push(new Project({ name: 'Task Tracker', description: "Task Tracker is a simple android application that aims to help the users keep a list of all the tasks they should complete while being as simple and non intrusive as possible.", repositoryUrl: 'https://github.com/HeinerMo/TaskTracker'}))
+    this.projects.push(new Project({ name: 'Questionnaire System', description: "A system that allows an organization to apply surveys to their employees and people outside the company's network.", repositoryUrl: 'https://github.com/HeinerMo/Questionnaires'}))
   }
 
   ngOnInit(): void {
