4dd02d75d6b6089574e7e9fb80c74c41eaddaa83
Heiner Monge <heinermongelopez@gmail.com>
2024-08-13 07:42:51 -0600

    Added multi-language support

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Technology } from 'src/app/models/Technology';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { HandsetData } from 'src/app/models/HandsetData';
+import { PageDataService } from 'src/services/PageDataService';
 
 @Component({
   selector: 'app-home',
@@ -11,12 +10,21 @@ import { HandsetData } from 'src/app/models/HandsetData';
 export class HomeComponent implements OnInit {
 
   handsetData: HandsetData = HandsetData.getInstance()
+  intro? : string;
+  title? : string;
   
-  constructor() { 
+  constructor(private pageDataService : PageDataService) {
 
   }
 
   ngOnInit(): void {
+    this.pageDataService.getValue('HomeIntroduction').subscribe(value => {
+      this.intro = value;
+    })
+
+    this.pageDataService.getValue('HomeTitle').subscribe(value => {
+      this.title = value;
+    })
   }
 
 }
