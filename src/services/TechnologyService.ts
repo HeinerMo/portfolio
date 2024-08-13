ed01f31f312bba0c166f5720155763aa2e8eefa6
Heiner Monge <heinermongelopez@gmail.com>
2024-08-13 07:43:43 -0600

    Added SiteCongif dependency

--- src/services/TechnologyService.ts
+++ src/services/TechnologyService.ts
@@ -3,14 +3,15 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Technology } from 'src/app/models/Technology';
+import { SiteConfig } from './SiteConfig';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TechnologyService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private config: SiteConfig) { }
 
   getTechnologies(): Observable<Technology[]> {
-    return this.http.get<Technology[]>('./assets/data/Technologies.json');
+      return this.http.get<Technology[]>('./assets/data/Technologies.json');
   }
 }
