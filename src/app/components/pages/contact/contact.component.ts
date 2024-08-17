da98faefe20d6f651dca077d07f92a6ab9423b15
Heiner Monge <heinermongelopez@gmail.com>
2024-08-17 10:44:49 -0600

    Multi-language support

--- src/app/components/pages/contact/contact.component.ts
+++ src/app/components/pages/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HandsetData } from 'src/app/models/HandsetData';
+import { PageDataService } from 'src/services/PageDataService';
 
 @Component({
   selector: 'app-contact',
@@ -10,10 +11,14 @@ export class ContactComponent implements OnInit {
 
 
   handsetData: HandsetData = HandsetData.getInstance()
+  title?: string
 
-  constructor() { }
+  constructor(private pageDataService: PageDataService) {}
 
   ngOnInit(): void {
+    this.pageDataService.getValue("ContactTitle").subscribe(value => {
+      this.title = value 
+    })
   }
 
 }
