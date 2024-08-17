d0491a96e1477e987206a767b4bbbce510512293
Heiner Monge <heinermongelopez@gmail.com>
2024-08-17 10:29:34 -0600

    Multi-language support

--- src/app/components/pages/projects/projects.component.ts
+++ src/app/components/pages/projects/projects.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HandsetData } from 'src/app/models/HandsetData';
 import { Project } from 'src/app/models/Project';
 import { PageDataService } from 'src/services/PageDataService';
+import { SiteConfig } from 'src/services/SiteConfig';
 
 @Component({
   selector: 'app-projects',
@@ -15,10 +16,22 @@ export class ProjectsComponent implements OnInit {
   title? : string
   intro? : string
 
-  constructor(private pageDataService: PageDataService) {
-    this.projects.push(new Project({ name: 'TC-645 Website', description: "A website is designed and developed for the TC-645 community work project at the University of Costa Rica. The website follows the visual identity standards of the university and complies with the regulations established therein.", repositoryUrl: 'https://tcu645.vercel.app/'}))
-    this.projects.push(new Project({ name: 'Task Tracker', description: "Task Tracker is a simple android application that aims to help the users keep a list of all the tasks they should complete while being as simple and non intrusive as possible.", repositoryUrl: 'https://github.com/HeinerMo/TaskTracker'}))
-    this.projects.push(new Project({ name: 'Questionnaire System', description: "A system that allows an organization to apply surveys to their employees and people outside the company's network.", repositoryUrl: 'https://github.com/HeinerMo/Questionnaires'}))
+  constructor(private pageDataService: PageDataService, public siteConfig: SiteConfig) {
+    this.projects.push(new Project({ 
+      name: 'TC-645 Website', 
+      descriptionEN: "A website is designed and developed for the TC-645 community work project at the University of Costa Rica. The website follows the visual identity standards of the university and complies with the regulations established therein.",
+      descriptionES: "Una página web para el trabajo comunal TC-645 de la Universidad de Costa Rica. La página sigue los estándares de identidad visual de la UCR y cumple con las regulaciones tablecidas en ellos.",
+      repositoryUrl: 'https://tcu645.vercel.app/'}))
+    this.projects.push(new Project({ 
+      name: 'Task Tracker',
+      descriptionEN: "A simple TO-DO app. A clasicc of the Junior Developer.", 
+      descriptionES: "Una aplicación de lista de tareas. Un clásico del desarrollador Junior",
+      repositoryUrl: 'https://github.com/HeinerMo/TaskTracker'}))
+    this.projects.push(new Project({ 
+      name: 'Questionnaire System', 
+      descriptionEN: "A system that allows an organization to apply surveys to their employees and people outside the company's network.", 
+      descriptionES: "Un sistema de cuestionarios que permite a la organización aplicar encuestas a empleados y a terceros",
+      repositoryUrl: 'https://github.com/HeinerMo/Questionnaires'}))
   }
 
   ngOnInit(): void {
--- src/app/models/Project.ts
+++ src/app/models/Project.ts
@@ -1,11 +1,13 @@
 export class Project {
     
     name?: String
-    description?: String
+    descriptionEN?: String
+    descriptionES? : String
     repositoryUrl?: String
     icon?: String
-    constructor({name, description, icon, repositoryUrl}: {name?: String, description?: String, icon?: String, repositoryUrl?: String}) {
-        this.description = description
+    constructor({name, descriptionEN, descriptionES, icon, repositoryUrl}: {name?: String, descriptionEN?: String, descriptionES?: String, icon?: String, repositoryUrl?: String}) {
+        this.descriptionEN = descriptionEN
+	this.descriptionES = descriptionES
         this.repositoryUrl = repositoryUrl
         this.name = name
         this.icon = icon
